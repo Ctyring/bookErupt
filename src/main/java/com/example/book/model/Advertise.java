5f3f7e383f960a2ac3e8df20fc8317dbd35dc581
Ctyring <75520657+Ctyring@users.noreply.github.com>
2022-04-18 15:34:38 +0800

    添加广告管理

---
+++ src/main/java/com/example/book/model/Advertise.java
@@ -0,0 +1,34 @@
+package com.example.book.model;
+
+import org.hibernate.annotations.GenericGenerator;
+import xyz.erupt.annotation.Erupt;
+import xyz.erupt.annotation.EruptField;
+import xyz.erupt.annotation.sub_erupt.Power;
+import xyz.erupt.annotation.sub_field.Edit;
+import xyz.erupt.annotation.sub_field.EditType;
+import xyz.erupt.annotation.sub_field.View;
+import xyz.erupt.annotation.sub_field.sub_edit.AttachmentType;
+import xyz.erupt.annotation.sub_field.sub_edit.InputType;
+import xyz.erupt.annotation.sub_field.sub_edit.Search;
+
+import javax.persistence.*;
+
+@Erupt(name = "Advertise", power = @Power(importable = true, export = true))
+@Table(name = "advertise")
+@Entity
+public class Advertise {
+
+    @Id
+    @GeneratedValue(generator = "generator")
+    @GenericGenerator(name = "generator", strategy = "native")
+    @Column(name = "id")
+    @EruptField
+    private Long id;
+
+    @EruptField(
+            views = @View(title = "图片"),
+            edit = @Edit(title = "图片", type = EditType.ATTACHMENT,
+                    attachmentType = @AttachmentType(type = AttachmentType.Type.IMAGE, maxLimit = 10))
+    )
+    private String picture;
+}
